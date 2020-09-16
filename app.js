ba2c9262a278cf995d6811791ffe419af1a71fb4
Dragos226 <62939083+Dragos226@users.noreply.github.com>
2020-09-16 19:01:06 +0300

    axZ

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const router = require("./routes/route");
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
+app.use(cors());
 app.use("/", router);
 app.listen(3000, () => console.log("HERE!!!"));
