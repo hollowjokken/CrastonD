53324d0e8c9bc667efa6f118e44e3d119b22d300
Dragos1221 <48069261+Dragos1221@users.noreply.github.com>
2020-09-19 15:04:34 +0300

    Connecting graphql to server.... (#1)
    * Connecting graphql to server
    
    Co-authored-by: Dragos226 <62939083+Dragos226@users.noreply.github.com>

--- app.js
+++ app.js
@@ -4,5 +4,6 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
+console.log("Craston ii place Sasu");
 app.use("/", router);
 app.listen(3000, () => console.log("Server is Runing!!!"));
