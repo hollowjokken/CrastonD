e2a9bc1bc3bda7a7ee405eb4234d38e958cf6793
Dragos226 <62939083+Dragos226@users.noreply.github.com>
2020-09-19 14:54:49 +0300

    Servev

--- app.js
+++ app.js
@@ -4,5 +4,6 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
+console.log("Crastin face saas");
 app.use("/", router);
 app.listen(3000, () => console.log("Server is Runing!!!"));
