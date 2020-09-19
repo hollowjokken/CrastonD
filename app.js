5867ca9a09a4db7de9cdfce655b05f41bb027370
Dragos226 <62939083+Dragos226@users.noreply.github.com>
2020-09-19 15:02:13 +0300

    Code review

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
-console.log("Crastin face saas");
+console.log("Craston ii place Sasu");
 app.use("/", router);
 app.listen(3000, () => console.log("Server is Runing!!!"));
